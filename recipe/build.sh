b7ba5c68545e6f2ae28c8f5486d268b5b083b2e0
Nehal J Wani <nehaljw.kkd1@gmail.com>
2025-04-21 19:02:30 +0100

    Specify strip program in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,8 +23,7 @@ fi
   --with-security-key-builtin \
   "${_xtra_configure_args[@]}"
 
-export STRIPPROG="$STRIP"
 
 make -j$CPU_COUNT
 # make install generate keys for the host which is not what we want
-make install-nokeys
+make install-nokeys STRIPPROG="$STRIP"
